4fba538a1e8e3a60cfa1ca886e4c6803090db5ae
Tam Vu <vutam_a@hotmail.com>
2019-11-27 14:37:55 +0000

    Automatically backed up by Learn

--- db/migrate/20191127143155_create_dogs.rb
+++ db/migrate/20191127143155_create_dogs.rb
@@ -10,3 +10,15 @@ class CreateDogs < ActiveRecord::Migration[5.2]
     drop_table :dogs
   end 
 end
+
+#can also refactor both method into change - down is implicit when rollback
+
+#class CreateDogs < ActiveRecord::Migration[5.2]
+#  def change
+#    create_table :dogs do |t|
+#      t.string :name
+#      t.string :breed
+#    end
+#  end
+# 
+#end
